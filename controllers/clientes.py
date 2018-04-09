621111127779a7dd9bd341e41161a9614e93f99f
Luis Cortes <luantrobest@gmail.com>
2018-04-09 02:39:41 -0500

    Primer commit en rama All_End

--- controllers/clientes.py
+++ controllers/clientes.py
@@ -3,3 +3,6 @@
 # This is a sample controller
 # this file is released under public domain and you can use without limitatio
 # -------------------------------------------------------------------------
+
+def function():
+	pass
