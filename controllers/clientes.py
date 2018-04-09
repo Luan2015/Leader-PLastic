e6530347c14cce68a07f209afc1c8eb69ebffec9
Luis Cortes <luantrobest@gmail.com>
2018-04-09 02:35:54 -0500

    Primer commit en rama Lain

---
+++ controllers/clientes.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# -------------------------------------------------------------------------
+# This is a sample controller
+# this file is released under public domain and you can use without limitatio
+# -------------------------------------------------------------------------
--- controllers/gestor.py
+++ controllers/gestor.py
@@ -9,6 +9,6 @@ def usuarios():
 	response.title = T("Usuarios")
 	pl_users = db.auth_user
 	pl_users.id.redable =  pl_users.id.writetable  = False				
-	links =  []
+	links =  ['ddddd']
 	pl_usuarios = SQLFORM.grid(pl_users,links=links,searchable=False,details=False,csv=False)
 	return locals()
